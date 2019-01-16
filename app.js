30ced1dcb6fdb00176f7aea08bb48b9ec31daa32
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-01-16 11:05:30 -0700

    created data.json and required in app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3002
 
+const data = require('./data.json')
+
+app.use(express.static('public'))
+
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
